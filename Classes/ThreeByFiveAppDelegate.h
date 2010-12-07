f699ec51e7f08f499047762a6c5bdc23a2153fc5
Zsolt Kiraly <zkiraly@zsolt-kiralys-macbook-pro.local>
2010-12-07 11:02:32 -0800

    Changed project name to ThreeByFive, renamed classes, and renamed target to 3x5.

---
+++ Classes/ThreeByFiveAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  TableViewTestAppDelegate.h
+//  TableViewTest
+//
+//  Created by Zsolt Kiraly on 10/2/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ThreeByFiveViewController;
+@class TableViewController;
+
+@interface ThreeByFiveAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    TableViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+//@property (nonatomic, retain) IBOutlet TableViewTestViewController *viewController;
+@property (nonatomic, retain) IBOutlet TableViewController *viewController;
+
+@end
+
